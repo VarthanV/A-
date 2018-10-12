e8248a09a68ec0601ec0c115a69e35ddec9ca7d3
VarthanV <varthanv26018@gmail.com>
2018-10-12 22:17:59 +0530

    Voice support added for my bae

--- A.py
+++ A.py
@@ -10,9 +10,12 @@ import random
 import webbrowser
 import time
 import sys
+import pyttsx3
+from weather import Weather,Unit
+from pyttsx3 import voice
 small_data={"What is your name":["My name is A"," Vichu named  me A but you can name me whatever you like",
                                  "A is how people call me"],
-            "Are you still single":["Yes he wants me to be like him as he is single",
+            "Are you  single":["Yes he wants me to be like him as he is single",
                                     "I'm not single I am surrounded by lines of code alwayss",
                                     "This  question is hypothetical","Being single is the  best"]
                                     
@@ -22,23 +25,45 @@ small_data={"What is your name":["My name is A"," Vichu named  me A but you can 
 positive_responses=['yes','yah','y','Y']
 negative_responses=['nah','no','n','N']
 
+engine=pyttsx3.init()
+voice=engine.getProperty('voices') # To bring the voice of my bae
+
+def say(msg):
+    engine.say(msg)
+    engine.runAndWait()
+
 default="Sorry I couldn't understand you since I am trained a little bit only"
 def ques():
             ques=input("Ask me your  questions i think i can answer")
+            say(ques)
+            
             if ques in small_data:
-                            print(random.choice(small_data[ques]))
+                            c=random.choice(small_data[ques])
+                        
+                            print(c)
+                            say(c)
+                            
+                            
+        
                 
             else:
                             print(default)
-      
-                        
+                            say(default)
+def weather():
+    weather=Weather(unit=Unit.CELSIUS)
+    location=weather.lookup_by_location('Coimbatore')
+    say(location.condition)
+def joke():
+    a=(i in range (100))
+    with open('Jokes.txt','r') as jok:
+        for line in jok:
+            text=(jok[random.choice(a)])
+            say(text)
             
-            
-                   
-def  access():
-    pass
-''' Updates yet to come'''
+        
     
+    
+                                   
             
                     
                     
@@ -46,20 +71,45 @@ def  access():
              
 def web():
          ans=input("Where you wanna search Youtube or web")
+         
+         say(ans)
          if(ans==1):
-            url=input("What sitee you want")
-            webbrowser.open("https://www.youtube.com/watch?v={}".format(url))
+             
+             url=input("What sitee you want")
+             say(url)
+             
+             webbrowser.open("https://www.youtube.com/watch?v={}".format(url))
+            
+            
+''' Main Code'''
+            
 a=input("Enter 1 to confirm that you are a human")
+say("Thanks for your patience and we are confirming your request")
+
 if(a=='1' or 1):
-    print("Validating pls wait")
-    time.sleep(5)
-    print("Hello, Iam A your simple commandline  assistant I do simple tasks I am yet to be developed")
-    response=input("Do you want to ask me something??")
-    if response in positive_responses:
-        ques()
-    else:
-        print("I am sorry take care")
-        sys.exit()
+            print("Welcome")
+            say("Welcome")
+        
+            time.sleep(5)
+            string="Hello, Iam A ,your simple commandline  assistant I do simple tasks I am yet to be developed"
+            print(string)
+            say(string)
+            response=input("Do you want to ask me something??")
+            say("Do you want to ask me something")
+            if response in positive_responses:
+                ques()
+            else:
+                apology="I am sorry take care"
+                print(apology)
+                say(apology) 
+                print(" You have other options like surfing web,weather checking etcc...")
+                ans=input("If  you wanna continue give your response or else give your response")
+                if(ans in positive_responses):
+                    web()
+                    
+                
+                sys.exit()
+
          
      
            
