90b0c7ca16bc598986592ecaf4bfbb62d690b374
VarthanV <varthanv26018@gmail.com>
2018-10-13 08:05:46 +0530

    Minor bug fixes

--- A.py
+++ A.py
@@ -15,7 +15,7 @@ from weather import Weather,Unit
 from pyttsx3 import voice
 small_data={"What is your name":["My name is A"," Vichu named  me A but you can name me whatever you like",
                                  "A is how people call me"],
-            "Are you  single":["Yes he wants me to be like him as he is single",
+            "Are you single":["Yes he wants me to be like him as he is single",
                                     "I'm not single I am surrounded by lines of code alwayss",
                                     "This  question is hypothetical","Being single is the  best"]
                                     
@@ -26,28 +26,30 @@ positive_responses=['yes','yah','y','Y']
 negative_responses=['nah','no','n','N']
 
 engine=pyttsx3.init()
-voice=engine.getProperty('voices') # To bring the voice of my bae
+Voice=engine.getProperty('voices') # To bring the voice of my bae
 
 def say(msg):
+    print(msg)
     engine.say(msg)
     engine.runAndWait()
 
 default="Sorry I couldn't understand you since I am trained a little bit only"
 def ques():
-            ques=input("Ask me your  questions i think i can answer")
-            say(ques)
+            sayer="Ask me your questions I will answer myy best"
+            say(sayer)
+            ques=input()
             
             if ques in small_data:
                             c=random.choice(small_data[ques])
                         
-                            print(c)
+                            
                             say(c)
                             
                             
         
                 
             else:
-                            print(default)
+                
                             say(default)
 def weather():
     weather=Weather(unit=Unit.CELSIUS)
@@ -75,11 +77,15 @@ def web():
          say(ans)
          if(ans==1):
              
-             url=input("What sitee you want")
-             say(url)
-             
-             webbrowser.open("https://www.youtube.com/watch?v={}".format(url))
+             url="What song you want"
             
+             say(url)
+             search=input()
+             
+             
+             webbrowser.open("https://www.youtube.com/watch?v={}".format(search))
+       
+           
             
 ''' Main Code'''
             
@@ -94,15 +100,18 @@ if(a=='1' or 1):
             string="Hello, Iam A ,your simple commandline  assistant I do simple tasks I am yet to be developed"
             print(string)
             say(string)
-            response=input("Do you want to ask me something??")
+            print("Do you want to ask me something??")
             say("Do you want to ask me something")
+            response=input()
             if response in positive_responses:
                 ques()
             else:
                 apology="I am sorry take care"
                 print(apology)
                 say(apology) 
-                print(" You have other options like surfing web,weather checking etcc...")
+                string=" You have other options like surfing web,weather checking etcc..."
+                
+                say(string)
                 ans=input("If  you wanna continue give your response or else give your response")
                 if(ans in positive_responses):
                     web()
